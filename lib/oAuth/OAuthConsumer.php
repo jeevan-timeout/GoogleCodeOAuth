e24a637e7a26d14633576c55cc0bcdf34bb1915f
Rajeevan <rajeevankumarathasan@timeout.com>
2011-10-13 18:45:10 +0100

    Init Files

---
+++ lib/oAuth/OAuthConsumer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace GoogleCodeOAuth\oAuth;
+
+class OAuthConsumer
+{
+
+    public $key;
+    public $secret;
+
+    function __construct( $key, $secret, $callback_url=NULL )
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+        $this->callback_url = $callback_url;
+    }
+
+    function __toString()
+    {
+        return "OAuthConsumer[key=$this->key,secret=$this->secret]";
+    }
+
+}
+
+?>
---
+++ lib/oAuth/OAuthException.php
@@ -0,0 +1,12 @@
+<?php
+namespace GoogleCodeOAuth\oAuth;
+
+/**
+ * Generic exception class
+ */
+class OAuthException extends Exception
+{
+    // pass
+}
+
+?>
